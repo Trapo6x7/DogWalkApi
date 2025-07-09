e627faac318888fbd07b748c364644c60988bdd9
Your Name <matoufdanslezoo@gmail.com>
2025-07-09 20:57:52 +0200

    TEST UNITAIRE

--- src/Security/Voter/GroupRequestVoter.php
+++ src/Security/Voter/GroupRequestVoter.php
@@ -11,17 +11,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 final class GroupRequestVoter extends Voter
 {
     public const READ = 'GROUPREQUEST_READ';
-    // private readonly GroupRoleRepository $groupRoleRepository;
-
-    // public function __construct(GroupRoleRepository $groupRoleRepository)
-    // {
-    //     $this->groupRoleRepository = $groupRoleRepository;
-    // }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
         return in_array($attribute, [self::READ])
             && $subject instanceof GroupRequest;
     }
@@ -29,7 +21,7 @@ final class GroupRequestVoter extends Voter
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
-        // if the user is anonymous, do not grant access
+
         if (!$user instanceof UserInterface) {
             return false;
         }
@@ -37,16 +29,10 @@ final class GroupRequestVoter extends Voter
         /** @var GroupRequest  $subject */
         $group = $subject->getWalkGroup();
 
-        // $walkGroupRole = $this->groupRoleRepository->findGroupRoleByGroupAndUser($group, $user)->getWalkGroup();
-
-        // if ($group !== $walkGroupRole){
-        //     return false;
-        // }
-        // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case self::READ:
                 foreach ($group->getGroupRoles() as $groupRole) {
-                    // dd($groupRole);
+               
                     if ($groupRole->getUser() == $user && $groupRole->getRole() == 'CREATOR') {
                         return true;
                     }
